01c726d49e7cd0a5ec69d1ed9d0f074082a3782f
BlockChainDev <kevtheappdev@gmail.com>
2019-08-05 19:28:12 +0100

    [Permutation]
    - Add constraint system counterpart for each stage in the permutation.
    This is further emphasized by splitting each stage into it's own
    `impl` section

--- src/permutation.rs
+++ src/permutation.rs
@@ -1,6 +1,7 @@
 use crate::errors::PermError;
 use crate::mds_matrix::*;
 use crate::round_constants::*;
+use bulletproofs::r1cs::{ConstraintSystem, LinearCombination, Variable};
 use curve25519_dalek::scalar::Scalar;
 use sha2::Sha512;
 
@@ -51,27 +52,6 @@ impl Permutation {
 
         Ok(perm)
     }
-    pub fn reset(&mut self) {
-        self.data.clear()
-    }
-    fn input_full(&self) -> bool {
-        self.data.len() == self.t
-    }
-    pub fn width_left(&self) -> usize {
-        self.t - self.data.len()
-    }
-    pub fn input_bytes(&mut self, bytes: &[u8]) -> Result<(), PermError> {
-        // Map bytes to group using elligator2
-        let scalar = Scalar::hash_from_bytes::<Sha512>(bytes);
-        self.input(scalar)
-    }
-    pub fn input(&mut self, scalar: Scalar) -> Result<(), PermError> {
-        if self.input_full() {
-            return Err(PermError::InputFull);
-        }
-        self.data.push(scalar);
-        Ok(())
-    }
     pub fn inputs(&mut self, scalars: Vec<Scalar>) -> Result<(), PermError> {
         let amount_to_add = scalars.len();
         let maximum_width = self.t;
@@ -84,53 +64,34 @@ impl Permutation {
         self.data.extend(scalars);
         Ok(())
     }
-    fn add_round_key(
-        &self,
-        constants: &mut RoundConstantsIterator,
-        words: Vec<Scalar>,
-    ) -> Result<Vec<Scalar>, PermError> {
-        words
-            .iter()
-            .map(|word| {
-                let c = constants.next().ok_or(PermError::NoMoreConstants)?;
-                Ok(word + c)
-            })
-            .collect()
-    }
-    fn apply_full_round(
-        &self,
-        constants: &mut RoundConstantsIterator,
-        words: Vec<Scalar>,
-    ) -> Result<Vec<Scalar>, PermError> {
-        // Add round keys to each word
-        let new_words = self.add_round_key(constants, words)?;
+}
 
-        // Then apply quintic s-box
-        let quintic_words: Result<Vec<Scalar>, PermError> = new_words
-            .iter()
-            .map(|word| Ok(Permutation::quintic_s_box(word)))
-            .collect();
-
-        // Multiply this result by the MDS matrix
-        Ok(self.matrix.mul_vector(&quintic_words?))
+// Utility methods on the permutation struct
+impl Permutation {
+    pub fn reset(&mut self) {
+        self.data.clear()
     }
-    fn quintic_s_box(scalar: &Scalar) -> Scalar {
-        scalar * scalar * scalar * scalar * scalar
+    fn input_full(&self) -> bool {
+        self.data.len() == self.t
     }
-
-    fn apply_partial_round(
-        &self,
-        constants: &mut RoundConstantsIterator,
-        words: Vec<Scalar>,
-    ) -> Result<Vec<Scalar>, PermError> {
-        // Add round keys to each word
-        let mut new_words = self.add_round_key(constants, words)?;
-        // Then apply quintic s-box to first element
-        new_words[0] = Permutation::quintic_s_box(&new_words[0]);
-        // Multiply this result by the MDS matrix
-        Ok(self.matrix.mul_vector(&new_words))
+    pub fn width_left(&self) -> usize {
+        self.t - self.data.len()
     }
+    pub fn input_bytes(&mut self, bytes: &[u8]) -> Result<(), PermError> {
+        // Map arbitrary bytes to group using elligator2
+        let scalar = Scalar::hash_from_bytes::<Sha512>(bytes);
+        self.input(scalar)
+    }
+    pub fn input(&mut self, scalar: Scalar) -> Result<(), PermError> {
+        if self.input_full() {
+            return Err(PermError::InputFull);
+        }
+        self.data.push(scalar);
+        Ok(())
+    }
+}
 
+impl Permutation {
     pub fn result(&self) -> Result<Vec<Scalar>, PermError> {
         let mut constants_iter = self.constants.iter();
 
@@ -152,5 +113,150 @@ impl Permutation {
         }
 
         Ok(new_words)
+    }
+
+    pub fn constrain_result(
+        &self,
+        cs: &mut dyn ConstraintSystem,
+        words: Vec<Variable>,
+    ) -> Result<Vec<LinearCombination>, PermError> {
+        let mut constants_iter = self.constants.iter();
+
+        let mut new_words: Vec<LinearCombination> = words.iter().map(|&var| var.into()).collect();
+
+        // Apply R_f full rounds
+        for _ in 0..self.full_rounds / 2 {
+            new_words = self.constrain_apply_full_round(&mut constants_iter, new_words, cs)?;
+        }
+
+        // Apply R_P partial rounds
+        for _ in 0..self.partial_rounds {
+            new_words = self.constrain_apply_partial_round(&mut constants_iter, new_words, cs)?;
+        }
+
+        // Apply R_f full rounds
+        for _ in 0..self.full_rounds / 2 {
+            new_words = self.constrain_apply_full_round(&mut constants_iter, new_words, cs)?;
+        }
+
+        Ok(new_words)
+    }
+}
+
+// Apply partial rounds
+impl Permutation {
+    fn apply_partial_round(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<Scalar>,
+    ) -> Result<Vec<Scalar>, PermError> {
+        // Add round keys to each word
+        let mut new_words = self.add_round_key(constants, words)?;
+        // Then apply quintic s-box to first element
+        new_words[0] = Permutation::quintic_s_box(&new_words[0]);
+        // Multiply this result by the MDS matrix
+        Ok(self.matrix.mul_vector(&new_words))
+    }
+    fn constrain_apply_partial_round(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<LinearCombination>,
+        cs: &mut dyn ConstraintSystem,
+    ) -> Result<Vec<LinearCombination>, PermError> {
+        // Add round keys to each word
+        let mut new_words = self.constrain_add_round_key(constants, words)?;
+        // Then apply quintic s-box to first element
+        new_words[0] = Permutation::constrain_quintic_s_box(new_words[0].clone(), cs);
+        // Multiply this result by the MDS matrix
+        Ok(self.matrix.constrain_mul_vector(new_words))
+    }
+}
+
+// Apply full round
+impl Permutation {
+    fn apply_full_round(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<Scalar>,
+    ) -> Result<Vec<Scalar>, PermError> {
+        // Add round keys to each word
+        let new_words = self.add_round_key(constants, words)?;
+
+        // Then apply quintic s-box
+        let quintic_words: Result<Vec<Scalar>, PermError> = new_words
+            .iter()
+            .map(|word| Ok(Permutation::quintic_s_box(word)))
+            .collect();
+
+        // Multiply this result by the MDS matrix
+        Ok(self.matrix.mul_vector(&quintic_words?))
+    }
+
+    fn constrain_apply_full_round(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<LinearCombination>,
+        cs: &mut dyn ConstraintSystem,
+    ) -> Result<Vec<LinearCombination>, PermError> {
+        // Add round keys to each word
+        let new_words = self.constrain_add_round_key(constants, words)?;
+
+        let quintic_words: Result<Vec<LinearCombination>, PermError> = new_words
+            .iter()
+            .map(|word| Ok(Permutation::constrain_quintic_s_box(word.clone(), cs)))
+            .collect();
+
+        // Multiply this result by the MDS matrix
+        Ok(self.matrix.constrain_mul_vector(quintic_words?))
+    }
+}
+
+// Add round key
+impl Permutation {
+    fn add_round_key(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<Scalar>,
+    ) -> Result<Vec<Scalar>, PermError> {
+        words
+            .iter()
+            .map(|word| {
+                let c = constants.next().ok_or(PermError::NoMoreConstants)?;
+                Ok(word + c)
+            })
+            .collect()
+    }
+
+    fn constrain_add_round_key(
+        &self,
+        constants: &mut RoundConstantsIterator,
+        words: Vec<LinearCombination>,
+    ) -> Result<Vec<LinearCombination>, PermError> {
+        words
+            .iter()
+            .map(|word| {
+                // First get the constant needed and convert it to a linear combination
+                let c = constants.next().ok_or(PermError::NoMoreConstants)?;
+                let c_lc = LinearCombination::from(c.clone());
+                // Return words + constants
+                Ok(word.clone() + c_lc)
+            })
+            .collect()
+    }
+}
+
+impl Permutation {
+    fn quintic_s_box(scalar: &Scalar) -> Scalar {
+        scalar * scalar * scalar * scalar * scalar
+    }
+    fn constrain_quintic_s_box(
+        lc: LinearCombination,
+        cs: &mut dyn ConstraintSystem,
+    ) -> LinearCombination {
+        let (lc, _, square) = cs.multiply(lc.clone(), lc);
+        let (_, _, quartic) = cs.multiply(square.into(), square.into());
+        let (_, _, quintic) = cs.multiply(quartic.into(), lc.into());
+
+        quintic.into()
     }
 }
