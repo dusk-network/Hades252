797b3c85a19edcd62e738c8f56bae371bdacd3c8
zer0 <matteo@dusk.network>
2019-09-25 17:17:06 +0200

    [WIP] Remove `Permutation` struct
    - Fix regression on `hash` to leave the first element (still need the
      proper value; it will be fixed on #17)
    
    See also: #19, #20

--- src/linear_combination/mod.rs
+++ src/linear_combination/mod.rs
@@ -7,133 +7,110 @@ use curve25519_dalek::scalar::Scalar;
 const TOTAL_FULL_ROUNDS: usize = 8;
 const PARTIAL_ROUNDS: usize = 59;
 
-pub struct Permutation<'a> {
-  // data to be used in the solid instantiation of the permutation struct
-  pub(crate) data: Vec<LinearCombination>,
-  pub(crate) cs: &'a mut dyn ConstraintSystem,
+fn perm(
+  cs: &mut dyn ConstraintSystem,
+  data: Vec<LinearCombination>,
+) -> Result<Vec<LinearCombination>, PermError> {
+  let mut constants_iter = ROUND_CONSTANTS.iter();
+
+  let mut new_words = data;
+
+  // Apply R_f full rounds
+  for _ in 0..TOTAL_FULL_ROUNDS / 2 {
+    new_words = apply_full_round(cs, &mut constants_iter, new_words)?;
+  }
+
+  // Apply R_P partial rounds
+  for _ in 0..PARTIAL_ROUNDS {
+    new_words = apply_partial_round(cs, &mut constants_iter, new_words)?;
+  }
+
+  // Apply R_f full rounds
+  for _ in 0..TOTAL_FULL_ROUNDS / 2 {
+    new_words = apply_full_round(cs, &mut constants_iter, new_words)?;
+  }
+
+  Ok(new_words)
 }
 
-impl<'a> Permutation<'a> {
-  #[allow(non_snake_case)]
-  pub fn new(cs: &'a mut dyn ConstraintSystem) -> Self {
-    Self {
-      data: Vec::with_capacity(MDS_MATRIX.len()),
-      cs,
-    }
-  }
+fn apply_partial_round<'a, I>(
+  cs: &mut dyn ConstraintSystem,
+  constants: &mut I,
+  words: Vec<LinearCombination>,
+) -> Result<Vec<LinearCombination>, PermError>
+where
+  I: Iterator<Item = &'a Scalar>,
+{
+  // Add round keys to each word
+  let mut new_words = add_round_key(constants, words)?;
+  // Then apply quintic s-box to first element
+  new_words[0] = quintic_s_box(cs, new_words[0].clone());
+  // Multiply this result by the MDS matrix
+  Ok(new_words * &MDS_MATRIX)
 }
 
-// Utility methods on the permutation struct
-impl Permutation<'_> {
-  pub fn input(&mut self, lc: LinearCombination) -> Result<(), PermError> {
-    if self.data.len() == MDS_MATRIX.len() {
-      return Err(PermError::InputFull);
-    }
-    self.data.push(lc);
-    Ok(())
-  }
-}
+fn apply_full_round<'a, I>(
+  cs: &mut dyn ConstraintSystem,
+  constants: &mut I,
+  words: Vec<LinearCombination>,
+) -> Result<Vec<LinearCombination>, PermError>
+where
+  I: Iterator<Item = &'a Scalar>,
+{
+  // Add round keys to each word
+  let new_words = add_round_key(constants, words)?;
 
-impl Permutation<'_> {
-  pub fn result(&mut self) -> Result<Vec<LinearCombination>, PermError> {
-    // Pad remaining width with zero
-    self.data.resize_with(MDS_MATRIX.len(), || Scalar::zero().into());
+  // Then apply quintic s-box
+  let quintic_words: Result<Vec<LinearCombination>, PermError> = new_words
+    .iter()
+    .map(|word| Ok(quintic_s_box(cs, word.clone())))
+    .collect();
 
-    let mut constants_iter = ROUND_CONSTANTS.iter();
-
-    let mut new_words: Vec<LinearCombination> = self.data.clone();
-
-    // Apply R_f full rounds
-    for _ in 0..TOTAL_FULL_ROUNDS / 2 {
-      new_words = self.apply_full_round(&mut constants_iter, new_words)?;
-    }
-
-    // Apply R_P partial rounds
-    for _ in 0..PARTIAL_ROUNDS {
-      new_words = self.apply_partial_round(&mut constants_iter, new_words)?;
-    }
-
-    // Apply R_f full rounds
-    for _ in 0..TOTAL_FULL_ROUNDS / 2 {
-      new_words = self.apply_full_round(&mut constants_iter, new_words)?;
-    }
-
-    Ok(new_words)
-  }
-}
-
-// Rounds
-impl Permutation<'_> {
-  fn apply_partial_round<'a, I>(
-    &mut self,
-    constants: &mut I,
-    words: Vec<LinearCombination>,
-  ) -> Result<Vec<LinearCombination>, PermError>
-  where
-    I: Iterator<Item = &'a Scalar>,
-  {
-    // Add round keys to each word
-    let mut new_words = self.add_round_key(constants, words)?;
-    // Then apply quintic s-box to first element
-    new_words[0] = self.quintic_s_box(new_words[0].clone());
-    // Multiply this result by the MDS matrix
-    Ok(new_words * &MDS_MATRIX)
-  }
-
-  fn apply_full_round<'a, I>(
-    &mut self,
-    constants: &mut I,
-    words: Vec<LinearCombination>,
-  ) -> Result<Vec<LinearCombination>, PermError>
-  where
-    I: Iterator<Item = &'a Scalar>,
-  {
-    // Add round keys to each word
-    let new_words = self.add_round_key(constants, words)?;
-
-    // Then apply quintic s-box
-    let quintic_words: Result<Vec<LinearCombination>, PermError> = new_words
-      .iter()
-      .map(|word| Ok(self.quintic_s_box(word.clone())))
-      .collect();
-
-    // Multiply this result by the MDS matrix
-    Ok(quintic_words? * &MDS_MATRIX)
-  }
+  // Multiply this result by the MDS matrix
+  Ok(quintic_words? * &MDS_MATRIX)
 }
 
 // Add round key
-impl Permutation<'_> {
-  fn add_round_key<'b, I>(
-    &self,
-    constants: &mut I,
-    words: Vec<LinearCombination>,
-  ) -> Result<Vec<LinearCombination>, PermError>
-  where
-    I: Iterator<Item = &'b Scalar>,
-  {
-    words
-      .iter()
-      .map(|word| {
-        let c = constants.next().ok_or(PermError::NoMoreConstants)?;
-        let c = LinearCombination::from(*c);
-        Ok(word.clone() + c)
-      })
-      .collect()
-  }
+fn add_round_key<'b, I>(
+  constants: &mut I,
+  words: Vec<LinearCombination>,
+) -> Result<Vec<LinearCombination>, PermError>
+where
+  I: Iterator<Item = &'b Scalar>,
+{
+  words
+    .iter()
+    .map(|word| {
+      let c = constants.next().ok_or(PermError::NoMoreConstants)?;
+      let c = LinearCombination::from(*c);
+      Ok(word.clone() + c)
+    })
+    .collect()
 }
 
-impl Permutation<'_> {
-  fn quintic_s_box(&mut self, lc: LinearCombination) -> LinearCombination {
-    let (lc, _, square) = self.cs.multiply(lc.clone(), lc);
-    let (_, _, quartic) = self.cs.multiply(square.into(), square.into());
-    let (_, _, quintic) = self.cs.multiply(quartic.into(), lc.into());
+fn quintic_s_box(cs: &mut dyn ConstraintSystem, lc: LinearCombination) -> LinearCombination {
+  let (lc, _, square) = cs.multiply(lc.clone(), lc);
+  let (_, _, quartic) = cs.multiply(square.into(), square.into());
+  let (_, _, quintic) = cs.multiply(quartic.into(), lc.into());
 
-    quintic.into()
-  }
+  quintic.into()
 }
 
-pub fn hash<'a>(mut perm: Permutation) -> Result<LinearCombination, PermError> {
-  let words = perm.result()?;
+pub fn hash(
+  cs: &mut dyn ConstraintSystem,
+  data: &[LinearCombination],
+) -> Result<LinearCombination, PermError> {
+  let width = MDS_MATRIX.len();
+
+  if data.len() >= width - 1 {
+    return Err(PermError::InputFull);
+  }
+
+  let mut words = vec![LinearCombination::from(Scalar::zero()); width];
+  let words_slice = &mut words[1..1 + data.len()];
+
+  words_slice.clone_from_slice(data);
+
+  let words = perm(cs, words)?;
   Ok(words[1].clone())
 }
