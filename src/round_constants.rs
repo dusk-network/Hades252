d3afb5a897dc72555dcc42432fcad4bf7db45f9e
zer0 <matteo@dusk.network>
2019-10-09 15:28:44 +0200

    Format source code with `rustfmt`
     Solves: #31

--- src/round_constants.rs
+++ src/round_constants.rs
@@ -1,6 +1,6 @@
-//! This module is designed to load from `ark.bin` the 960 
+//! This module is designed to load from `ark.bin` the 960
 //! constants used as `round_constants` in a `lazy_static` module.
-//! 
+//!
 //! The constants were originally computed using:
 //! https://extgit.iaik.tugraz.at/krypto/hadesmimc/blob/master/code/calc_round_numbers.py
 //! and then mapped onto `Scalar` in the Ristretto scalar field.
@@ -12,10 +12,10 @@ use lazy_static::*;
 lazy_static! {
   /// `ROUND_CONSTANTS` constists on a static reference
   /// that points to the pre-loaded 960 Scalar constants.
-  /// 
+  ///
   /// This 960 `Scalar` constants are loaded from `ark.bin`
   /// where all of the `Scalar` are represented in bytes.
-  /// 
+  ///
   /// This round constants have been taken from:
   /// https://extgit.iaik.tugraz.at/krypto/hadesmimc/blob/master/code/calc_round_numbers.py
   /// and then mapped onto `Scalar` in the Ristretto scalar field.
@@ -28,13 +28,13 @@ lazy_static! {
 
 #[cfg(test)]
 mod test {
-  use super::*;
+    use super::*;
 
-  #[test]
-  fn test_round_constants() {
-    // Check each element is non-zero
-    let zero = Scalar::zero();
-    let has_zero = ROUND_CONSTANTS.iter().any(|&x| x == zero);
-    assert!(!has_zero);
-  }
+    #[test]
+    fn test_round_constants() {
+        // Check each element is non-zero
+        let zero = Scalar::zero();
+        let has_zero = ROUND_CONSTANTS.iter().any(|&x| x == zero);
+        assert!(!has_zero);
+    }
 }
