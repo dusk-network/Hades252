2abcb56b101b3bb6290dcffbdafee7d61e6508a9
Victor Lopez <victor@dusk.network>
2020-03-03 19:14:28 +0100

    Pre-build constants and MDS cauchy matrix

--- build/build.rs
+++ build/build.rs
@@ -1,3 +1,10 @@
+mod ark;
+mod mds;
+
+pub use algebra::{curves::bls12_381::Bls12_381 as Curve, fields::bls12_381::fr::Fr as Scalar};
+
 fn main() -> std::io::Result<()> {
+    ark::write_to("assets/ark.bin")?;
+    mds::write_to("assets/mds.bin")?;
     Ok(())
 }
